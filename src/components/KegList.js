9c5d44486b0523c6127fca9b21eef8959a4bdd54
Matt Stroud <lixingke06@gmail.com>
2020-07-03 10:31:53 -0700

    map all kegs from list to display

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,29 +1,29 @@
 import React from "react";
 import Keg from "./Keg";
+import PropTypes from "prop-types";
 import { Row, Col, Card } from "reactstrap";
 
-function KegList() {
+function KegList(props) {
   return (
     <React.Fragment>
       <Col md="12">
-        <Row>
-          <Card>
-            <Keg />
-          </Card>
-        </Row>
-        <Row>
-          <Card>
-            <Keg />
-          </Card>
-        </Row>
-        <Row>
-          <Card>
-            <Keg />
-          </Card>
-        </Row>
+        {props.kegList.map((keg) =>
+          <Row key={"row" + keg.id}>
+            <Card key={"card" + keg.id}>
+              <Keg name={keg.name}
+                brand={keg.brand}
+                price={keg.price}
+                key={keg.id} />
+            </Card>
+          </Row>
+        )}
       </Col>
     </React.Fragment>
   );
+}
+
+KegList.propTypes = {
+  kegList: PropTypes.array
 }
 
 export default KegList;
