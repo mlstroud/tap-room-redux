0868e0f4cc4d3f5db8cf739bae8bb39af787fadf
Matt Stroud <lixingke06@gmail.com>
2020-07-03 09:58:32 -0700

    add button handler to update formVisisbleOnPage state

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -3,6 +3,7 @@ import KegList from "./KegList";
 import NewKegForm from "./NewKegForm";
 import KegDetail from "./KegDetail";
 import EditKegForm from "./EditKegForm";
+import { Button } from "reactstrap";
 
 class KegControl extends React.Component {
 
@@ -16,18 +17,25 @@ class KegControl extends React.Component {
     };
   }
 
+  handleClick = () => {
+    this.setState({ formVisibleOnPage: true });
+  }
+
   render() {
     let currentlyVisibleState = null;
+    let addKegButton = null;
 
     if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewKegForm />
     } else {
       currentlyVisibleState = <KegList />
+      addKegButton = <Button onClick={this.handleClick}>Add Keg</Button>
     }
 
     return (
       <React.Fragment>
         {currentlyVisibleState}
+        {addKegButton}
       </React.Fragment>
     );
   }
