d4a177479d65e07f750800373aeef5b120f3987e
Matt Stroud <lixingke06@gmail.com>
2020-07-03 16:31:50 -0700

    add header image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Jumbotron } from "reactstrap";
+import BannerImg from "./../img/banner.png";
 
 function Header() {
   return (
     <React.Fragment>
       <Jumbotron>
-        <h1>Tap Room</h1>
+        <img src={BannerImg} className="banner-img" />
+        <h1>Epibeerus Tap Room</h1>
       </Jumbotron>
     </React.Fragment>
   );
