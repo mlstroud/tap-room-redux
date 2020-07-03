48354c6e8f02527f6999f4fb703ca9dcb4f9380d
Matt Stroud <lixingke06@gmail.com>
2020-07-03 10:31:12 -0700

    add form and handler for adding new keg

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,9 +1,43 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { v4 } from "uuid";
+import { Form, Input, Label, Button } from "reactstrap";
 
-function NewKegForm() {
+function NewKegForm(props) {
+
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    props.onNewKegCreation({
+      name: event.target.kegName.value,
+      brand: event.target.kegBrand.value,
+      price: event.target.kegPrice.value,
+      id: v4()
+    })
+  }
+
   return (
-    "New Keg Form"
+    <React.Fragment>
+      <Form onSubmit={handleNewKegFormSubmission}>
+        <Label for="kegName">Name</Label>
+        <Input type="text"
+          name="kegName"
+          placeholder="Tasty IPA" />
+        <Label for="kegBrand">Brand</Label>
+        <Input type="text"
+          name="kegBrand"
+          placeholder="Tasty Brewing Co." />
+        <Label for="kegPrice">Price</Label>
+        <Input type="text"
+          name="kegPrice"
+          placeholder="6.99" />
+        <Button type="submit">Add</Button>
+      </Form>
+    </React.Fragment>
   );
+}
+
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
 }
 
 export default NewKegForm;
