c893fc925a1222b911e9e0fb1402cfc0079d0ffd
Matt Stroud <lixingke06@gmail.com>
2020-07-03 10:30:40 -0700

    add props and display styling

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,9 +1,36 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { CardBody, CardTitle, Table, } from "reactstrap";
 
-function Keg() {
+
+function Keg(props) {
   return (
-    "Keg"
+    <React.Fragment>
+      <CardBody>
+        <CardTitle><h4>{props.name}</h4></CardTitle>
+        <Table>
+          <tbody>
+            <tr>
+              <th>Brand</th>
+              <td>{props.brand}</td>
+            </tr>
+            <tr>
+              <th>Price</th>
+              <td>{props.price}</td>
+            </tr>
+          </tbody>
+        </Table>
+
+      </CardBody>
+    </React.Fragment>
   );
+}
+
+Keg.propTypes = {
+  name: PropTypes.string,
+  brand: PropTypes.string,
+  price: PropTypes.number,
+  id: PropTypes.string
 }
 
 export default Keg;
