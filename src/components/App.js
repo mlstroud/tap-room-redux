151122f0031c8cdae8cd8a79b7cfe43a98a3d2c3
Matt Stroud <lixingke06@gmail.com>
2020-07-03 09:11:17 -0700

    add KegControl component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import KegControl from "./KegControl";
 
 function App() {
   return (
---
+++ src/components/KegControl.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class KegControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      "KegControl"
+    );
+  }
+}
+
+export default KegControl;
