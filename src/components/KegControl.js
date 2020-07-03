881ff046f81e595d7d82ec57a6362052adb64468
Matt Stroud <lixingke06@gmail.com>
2020-07-03 09:19:19 -0700

    add NewKegForm component

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -1,10 +1,14 @@
 import React from "react";
+import NewKegForm from "./NewKegForm";
 
 class KegControl extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      masterKegList: [],
+      formVisibleOnPage: false
+    };
   }
 
   render() {
---
+++ src/components/NewKegForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function NewKegForm() {
+  return (
+    "New Keg Form"
+  );
+}
+
+export default NewKegForm;
