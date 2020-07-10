e01743b96f7b96163d3cc42c26c6c1e9c57cab8d
Matt Stroud <lixingke06@gmail.com>
2020-07-10 08:53:29 -0700

    add test for toggling edit state

--- src/__tests__/reducers/editing-reducer.test.js
+++ src/__tests__/reducers/editing-reducer.test.js
@@ -4,4 +4,8 @@ describe("editingReducer", () => {
   test("Should return default state if there is matching action type.", () => {
     expect(editingReducer(false, { type: null })).toEqual(false);
   });
+
+  test("Should toggle editing state to true.", () => {
+    expect(editingReducer(false, { type: "TOGGLE_EDIT" })).toEqual(true);
+  });
 });
