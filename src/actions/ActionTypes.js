5b65d10be6af5a635cfebdeac83f29b9038c2de2
Matt Stroud <lixingke06@gmail.com>
2020-07-10 12:50:58 -0700

    add action types

---
+++ src/actions/ActionTypes.js
@@ -0,0 +1,6 @@
+export const ADD_KEG = "ADD_KEG";
+export const DELETE_KEG = "DELETE_KEG";
+export const SELL_PINT = "SELL_PINT";
+export const TOGGLE_FORM = "TOGGLE_FORM";
+export const TOGGLE_EDIT = "TOGGLE_EDIT";
+export const SELECT_KEG = "SELECT_KEG";
