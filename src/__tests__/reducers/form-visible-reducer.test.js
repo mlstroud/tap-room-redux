023077bcbf4367d9cfcdf037c1a1ce195db7c82c
Matt Stroud <lixingke06@gmail.com>
2020-07-10 08:56:22 -0700

    add test for toggling form state to true

--- src/__tests__/reducers/form-visible-reducer.test.js
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -5,4 +5,8 @@ describe("formReducer", () => {
   test("Should return default state if there is no matching action type", () => {
     expect(formReducer(false, { type: null })).toEqual(false);
   });
+
+  test("Should toggle form state to true", () => {
+    expect(formReducer(false, { type: "TOGGLE_FORM" })).toEqual(true);
+  });
 });
