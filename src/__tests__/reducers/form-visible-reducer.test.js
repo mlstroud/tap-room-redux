cfcc86509763b08eaa987ce90289edc159e3fdb5
Matt Stroud <lixingke06@gmail.com>
2020-07-10 08:54:45 -0700

    add reducer and test for default state

---
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -0,0 +1,8 @@
+import formReducer from "../../reducers/form-visible-reducer";
+
+describe("formReducer", () => {
+
+  test("Should return default state if there is no matching action type", () => {
+    expect(formReducer(false, { type: null })).toEqual(false);
+  });
+});
