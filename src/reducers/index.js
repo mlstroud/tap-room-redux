1b68801b79e1ecc87dbb769a444465a2b6d9d535
Matt Stroud <lixingke06@gmail.com>
2020-07-10 10:30:31 -0700

    add root reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,4 @@
+import formReducer from "./form-visible-reducer";
+import kegListReducer from "./keg-list-reducer";
+import editReducer from "./editing-reducer";
+import selectedKegReducer from "./selected-keg-reducer";
