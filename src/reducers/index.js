1a4c09159a24fd5cfe28f9c9ed697193ffb0c586
Matt Stroud <lixingke06@gmail.com>
2020-07-10 10:44:01 -0700

    combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,3 +2,13 @@ import formReducer from "./form-visible-reducer";
 import kegListReducer from "./keg-list-reducer";
 import editReducer from "./editing-reducer";
 import selectedKegReducer from "./selected-keg-reducer";
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  masterKegList: kegListReducer,
+  selectedKeg: selectedKegReducer,
+  formVisible: formReducer,
+  editing: editReducer
+});
+
+export default rootReducer;
