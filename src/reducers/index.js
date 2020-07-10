1ca3f5b05f4cfb632dec19ec569ec92cac41d04c
Matt Stroud <lixingke06@gmail.com>
2020-07-10 11:32:49 -0700

    created custom combineReducer function to share state between needed slices

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,11 +4,41 @@ import editReducer from "./editing-reducer";
 import selectedKegReducer from "./selected-keg-reducer";
 import { combineReducers } from "redux";
 
-const rootReducer = combineReducers({
-  masterKegList: kegListReducer,
-  selectedKeg: selectedKegReducer,
-  formVisible: formReducer,
-  editing: editReducer
-});
+// const rootReducer = combineReducers({
+//   masterKegList: kegListReducer,
+//   selectedKeg: selectedKegReducer(masterKegList),
+//   formVisible: formReducer,
+//   editing: editReducer
+// });
+
+const rootReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_KEG":
+    case "DELETE_KEG":
+    case "SELL_PINT":
+      return {
+        masterKegList: kegListReducer(state, action)
+      }
+    case "SELECT_KEG":
+      return {
+        selectedKeg: selectedKegReducer(state.masterKegList, action)
+      }
+    case "TOGGLE_EDIT":
+      return {
+        editing: editReducer(state, action)
+      }
+    case "TOGGLE_FORM":
+      return {
+        formVisible: formReducer(state, action)
+      }
+    default:
+      return {
+        masterKegList: kegListReducer(state, action),
+        selectedKeg: selectedKegReducer(state.masterKegList, action),
+        editing: editReducer(state.editing, action),
+        formVisible: formReducer(state.formVisible, action)
+      }
+  }
+}
 
 export default rootReducer;
