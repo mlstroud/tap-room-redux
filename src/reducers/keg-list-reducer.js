d13802d41c0636657c340e0a48761097b3b78a6e
Matt Stroud <lixingke06@gmail.com>
2020-07-10 09:08:12 -0700

    update reducer to add new keg to master list

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,20 @@
 export default (state = {}, action) => {
-  return state;
+
+  const { name, brand, price, abv, pints, id } = action;
+
+  switch (action.type) {
+    case "ADD_KEG":
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          brand: brand,
+          price: price,
+          abv: abv,
+          pints: pints,
+          id: id
+        }
+      });
+    default:
+      return state;
+  }
 }
