8e561a2156d7a914d7669b0b73e886c4496b1d6e
Matt Stroud <lixingke06@gmail.com>
2020-07-10 09:41:53 -0700

    update reducer to update pint quanity when sold

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -18,6 +18,17 @@ export default (state = {}, action) => {
       const newState = { ...state };
       delete newState[id];
       return newState;
+    case "SELL_PINT":
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          brand: brand,
+          price: price,
+          abv: abv,
+          pints: pints - 1,
+          id: id
+        }
+      });
     default:
       return state;
   }
