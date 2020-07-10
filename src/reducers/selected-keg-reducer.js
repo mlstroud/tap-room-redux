005aba7d226a0a92d2e496dc9c1e43a91beb1e1c
Matt Stroud <lixingke06@gmail.com>
2020-07-10 10:08:46 -0700

    update reducer to select key by id

--- src/reducers/selected-keg-reducer.js
+++ src/reducers/selected-keg-reducer.js
@@ -1,3 +1,12 @@
 export default (state = {}, action) => {
-  return state;
+
+  const { id } = action;
+
+  switch (action.type) {
+    case "SELECT_KEG":
+      const selectedKeg = state[id];
+      return selectedKeg;
+    default:
+      return state;
+  }
 }
